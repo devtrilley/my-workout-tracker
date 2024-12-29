9d23780922d75ccfe72c43d2fec973ee381b3925
Tom Rilley <tomrilley@Mac.attlocal.net>
2024-12-29 13:13:20 -0500

    Added comments to files. Added gitignore, ignoring .env and node_modules

---
+++ server.js
@@ -0,0 +1,3 @@
+// server.js is to Create and Configure the Server. Entry point for our backend application
+// This is where we'll define and configure your server, handle incoming requests, and interact with the database.
+
