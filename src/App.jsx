4ab7b6ab9e4ba4e788f00c826c0c95de825f00dd
Tom Rilley <tamsirrilley@gmail.com>
2025-01-18 19:06:50 -0500

    crated header and footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
+import React from 'react';
+import Header from './components/header/header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // Returns the entire UI
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // Div holding our header and text
+    <div>
+      <Header />
+      <p>Track your workouts easily and effectively!</p>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"; // Imports CSS
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Hevy Workout Tracker</h1>
+    </header>
+  );
+}
+
+export default Header;
